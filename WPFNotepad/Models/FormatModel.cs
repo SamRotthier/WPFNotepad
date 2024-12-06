92116d83584acfe77bb639a382ae7ae97dce68d3
Sam Rotthier <sam.rotthier@yitch.eu>
2024-12-06 19:45:57 +0100

    Add project files.

---
+++ WPFNotepad/Models/FormatModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WPFNotepad.Models
+{
+    public class FormatModel : ObservabltObject
+    {
+        private FontStyle _style;
+        public FontStyle Style
+        {
+            get { return _style; }
+            set { OnPropertyChanged(ref _style, value); }
+        }
+
+        private FontWeight _weight;
+        public FontWeight Weight
+        {
+            get { return _weight; }
+            set { OnPropertyChanged(ref _weight, value); }
+        }
+
+        private FontFamily _family;
+        public FontFamily Family
+        {
+            get { return _family; }
+            set { OnPropertyChanged(ref _family, value); }
+        }
+
+        private TextWrapping _wrap;
+        public TextWrapping Wrap
+        {
+            get { return _wrap; }
+            set 
+            { 
+                OnPropertyChanged(ref _wrap, value); 
+                isWrapped = value == TextWrapping.Wrap? true : false;
+            }
+        }
+    }
+}
---
+++ WPFNotepad/ObservabltObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNotepad
+{
+    public class ObservabltObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged<T>(ref T property, T value, [CallerMemberName] string propertyName = "") //CallerMemberName will pick up the type so you don't have to use a magic string
+        {
+            property = value;
+            var handler = PropertyChanged;
+            if(handler != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        
+        }
+    }
+}
